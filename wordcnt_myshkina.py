6c8e9fb93fe0ef3c6448201761791c296bec98cf
dmako8 <dmako@stevens.edu>
2021-04-15 14:20:26 -0400

    Changed comment format

--- wordcnt_myshkina.py
+++ wordcnt_myshkina.py
@@ -1,15 +1,19 @@
 def word_count():
-
+   
+    #creates a list (array) of lines of the text
     with open('alice.txt') as file_object:
-        lines = file_object.readlines()  #creates a list (array) of lines of the text
-
-    new_string = '' #initializing a new empty string
+        lines = file_object.readlines()  
+    
+    #initializing a new empty string
+    new_string = '' 
+    
+    #removes the whitespace at the end of every line 
+    #and adds that line to the string
     for line in lines:
         new_string += line.rstrip() 
-        #removes the whitespace at the end of every line 
-        #and adds that line to the string
-
-    return new_string.count(" ") #counts the number of spaces in the string
-    #the number of spaces is approximately the same as the number of words
+       
+    #counts the number of spaces in the string the number of spaces is
+    # approximately the same as the number of words
+    return new_string.count(" ") 
 
 print("The file alice.txt has about", word_count(), "words.")
